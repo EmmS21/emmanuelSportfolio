b62c38ad116b1dd11b14016ff331aecd406fc070
EmmS21 <44911198+EmmS21@users.noreply.github.com>
2022-10-29 13:36:47 -0500

    Updates

---
+++ precache-manifest.c1002c3ebbccbdd0d55cb4b57096bfa1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f909997c0409de43e417b53de380c03f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "95dcf99be28ada247c0a",
+    "url": "/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "cd3de5907cfb0b74d584",
+    "url": "/static/css/main.db76a3b0.chunk.css"
+  },
+  {
+    "revision": "95dcf99be28ada247c0a",
+    "url": "/static/js/2.2c618cec.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/static/js/2.2c618cec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd3de5907cfb0b74d584",
+    "url": "/static/js/main.8afb676d.chunk.js"
+  },
+  {
+    "revision": "a2d41c56873c741b51b2",
+    "url": "/static/js/runtime-main.a5042f6b.js"
+  }
+]);
