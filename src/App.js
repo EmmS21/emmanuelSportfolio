6e24afc0dd07f2b096d580d83fc8f1cabc0df5e6
EmmS21 <44911198+EmmS21@users.noreply.github.com>
2023-01-30 09:05:12 -0600

    removed dark mode

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import About from "./components/About";
 import Experience from "./components/Experience";
 import Projects from "./components/Projects";
 import Skills from "./components/Skills";
+
 class App extends Component {
 
   constructor(props) {
